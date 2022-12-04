61c4fe43cc203026c13c83574d715900c094d5c7
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-12-04 15:19:09 -0800

    r

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 import setuptools
 
-import ciberedev
-
 with open("README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
 
@@ -18,11 +16,11 @@ setuptools.setup(
         "Issue tracker": "https://github.com/cibere/ciberedev.py/issues",
         "Discord/Support Server": "https://discord.gg/2MRrJvP42N",
     },
-    version=ciberedev.__version__,
+    version="0.1.10124",
     python_requires=">=3.8",
     install_requires=REQUIREMENTS,
     packages=["ciberedev"],
-    description=ciberedev.__description__,
+    description="its a desc",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     license="MIT",
